a21e31cf09181ab0638ab4f27ddd814a8f707c51
Yusuf Akorede <koredey4u@gmail.com>
2025-09-21 01:58:47 +0100

    new background for stairs

--- components/DownloadResumeButton.tsx
+++ components/DownloadResumeButton.tsx
@@ -1,14 +1,18 @@
 "use client";
 
+import { useState } from "react";
 import { FiDownload } from "react-icons/fi";
 import { Button } from "./ui/button";
 
 export default function DownloadResumeButton() {
+  const [isDownloading, setIsDownloading] = useState(false);
   const handleDownload = () => {
+    setIsDownloading(true);
     const link = document.createElement("a");
     link.href = "/resume.pdf";
     link.download = "Yusuf-Akorede-Resume.pdf"; // optional custom name
     link.click();
+    setIsDownloading(false);
   };
 
   return (
@@ -18,7 +22,7 @@ export default function DownloadResumeButton() {
       size="lg"
       className="uppercase flex items-center gap-2"
     >
-      <span>Download CV</span>
+      <span>{isDownloading ? "Downloading" : "Download"} CV</span>
       <FiDownload className="text-xl" />
     </Button>
   );
--- components/Stairs.tsx
+++ components/Stairs.tsx
@@ -28,7 +28,7 @@ const Stairs = () => {
               ease: "easeInOut",
               delay: reverseIndex(index) * 0.1,
             }}
-            className="h-full w-full bg-white relative"
+            className="h-full w-full bg-accent-hover/20 relative"
           />
         );
       })}
