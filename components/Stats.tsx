dd4b05c06bfc18fd55787d16989fb96f7320faac
Yusuf Akorede <koredey4u@gmail.com>
2025-09-21 02:02:22 +0100

    push

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -3,10 +3,10 @@
 import CountUp from "react-countup";
 
 const stats = [
-  { num: 12, text: "Years of experience" },
-  { num: 26, text: "Projects completed" },
-  { num: 8, text: "Technologies mastered" },
-  { num: 500, text: "Code commites" },
+  { num: 3, text: "Years of experience" },
+  { num: 20, text: "Projects completed" },
+  { num: 10, text: "Technologies mastered" },
+  { num: 1000, text: "Code commites" },
 ];
 
 const Stats = () => {
